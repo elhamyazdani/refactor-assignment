caeb71b91094c17a00d92a971f83e63422b739e6
Elham Yazdani <el.yazdani94@gmail.com>
2022-07-06 13:54:02 +0430

    [refactor] modify loop in <ProductList>

--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import lodash from 'lodash'
 import Product from './Product'
+import ProductInterface from '../../models/product.model'
 
-interface IPostsProps {
+interface PostsProps {
   products: any
   onFav: (title: string) => void
 }
 
-const ProductsList: React.FC<IPostsProps> = ({products, onFav}) => {
-  let productsarr = []
-  for (const [i, p] of products.entries()) {
-    productsarr.push(
-      <Product key={i} product={p} onFav={onFav} />
-    )
-  }
-  return <div>{lodash.reverse(productsarr)}</div>
+const ProductsList: React.FC<PostsProps> = ({ products, onFav }) => {
+
+    //TODO Make products upside down
+  return (
+      <div>
+      {products.map((product: ProductInterface, index: number) => (
+        <Product key={index} product={product} onFav={onFav} />
+      ))}
+    </div>
+  )
 }
 export default ProductsList
