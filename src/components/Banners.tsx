9ddea10f3230bd5211591484742ec27419558ca8
Elham Yazdani <el.yazdani94@gmail.com>
2022-07-06 16:55:29 +0430

    [refactor] modify <Banners> and add style

--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -1,23 +1,14 @@
 import React from 'react'
-import styles from '../shopApp.module.css'
+import styles from './banners.module.css'
 import img1 from '../images/img1.png'
 import img2 from '../images/img2.png'
 
 const Banners: React.FC = () => {
   return (
-    <>
-      <span
-        className={['container', styles.main].join(' ')}
-        style={{
-          margin: '50px inherit',
-          display: 'flex',
-          justifyContent: 'space-evenly'
-        }}
-      >
-        <img src={img1} style={{ maxHeight: '15em', display: 'block' }} />
-        <img src={img2} style={{ maxHeight: '15rem', display: 'block' }} />
-      </span>
-    </>
+    <div className={styles.wrapper}>
+      <img src={img1} className={styles.banner} />
+      <img src={img2} className={styles.banner} />
+    </div>
   )
 }
 
