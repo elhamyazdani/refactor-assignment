d6b25ed54e96aa542cc53d9e14a8f080a99fdc63
Elham Yazdani <el.yazdani94@gmail.com>
2022-07-07 15:09:23 +0430

    [test] add test of required fields  in AddProductForm

--- src/components/product/AddProductFrom.test.tsx
+++ src/components/product/AddProductFrom.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 import AddProductForm from './AddProductForm'
+import user from '@testing-library/user-event'
 
 describe('Add Product Form', () => {
   beforeEach(() => {
@@ -19,6 +20,26 @@ describe('Add Product Form', () => {
     expect(getDescription()).toBeInTheDocument()
     expect(getButton()).toBeInTheDocument()
   })
+  it('onSubmit should not happen when the title field is not filled', () => {
+    user.type(getPrice(), 'Test price')
+    user.type(getDescription(), 'Test description as a textbox it is longer')
+    user.click(getButton())
+    expect(getError()).toBeInTheDocument()
+  })
+
+  it('onSubmit should not happen when the price field is not filled', () => {
+    user.type(getTitle(), 'Test title')
+    user.type(getDescription(), 'Test description as a textbox it is longer')
+    user.click(getButton())
+    expect(getError()).toBeInTheDocument()
+  })
+
+  it('onSubmit should happen when the description field in not filled', () => {
+    user.type(getTitle(), 'Test title')
+    user.type(getPrice(), 'Test price')
+    user.click(getButton())
+    expect(getError()).toBeInTheDocument()
+  })
 })
 
 function getTitle () {
@@ -32,4 +53,7 @@ function getDescription () {
 }
 function getButton () {
   return screen.getByRole('button')
+}
+function getError () {
+  return screen.getByText(/fill the required fields/i)
 }
